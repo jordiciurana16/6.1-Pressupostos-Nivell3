d48d2df64bccdd5a4b64627639ac7c4b5b864ca5
jordiciurana16 <jciuranas@gmail.com>
2025-03-31 12:31:36 +0200

    render data from cards

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ICard } from './i-card';
+import cardJSON from './cards.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   cardList: ICard[] = [];
+  constructor() {
+    this.cardList = cardJSON;
+  }
 }
