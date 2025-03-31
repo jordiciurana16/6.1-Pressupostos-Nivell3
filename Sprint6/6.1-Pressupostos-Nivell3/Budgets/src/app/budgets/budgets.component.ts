b6eb9492b4cce229bbf2ab74026279007f5958df
jordiciurana16 <jciuranas@gmail.com>
2025-03-31 12:49:29 +0200

    budget price

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/budgets/budgets.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/budgets/budgets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { FinalBudgetComponent } from '../final-budget/final-budget.component';
 
 @Component({
   selector: 'app-budgets',
-  imports: [CardComponent],
+  imports: [CardComponent, FinalBudgetComponent],
   templateUrl: './budgets.component.html',
   styleUrl: './budgets.component.scss',
 })
---
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/final-budget/final-budget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-final-budget',
+  imports: [],
+  templateUrl: './final-budget.component.html',
+  styleUrl: './final-budget.component.scss'
+})
+export class FinalBudgetComponent {
+
+}
