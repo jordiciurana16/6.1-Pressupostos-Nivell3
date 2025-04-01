b6bed91de1dc7f7c9a9b0dad1fa818b6335ab317
jordiciurana16 <jciuranas@gmail.com>
2025-04-01 12:15:50 +0200

    card reactive forms & budget list

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
@@ -2,16 +2,57 @@ import { Component } from '@angular/core';
 import { ICard } from './i-card';
 import cardJSON from './cards.json';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   cardList: ICard[] = [];
+
+  budgetList: Array<number> = [];
+  removedIndex: number | undefined;
+
+  checkboxArray = new FormArray<FormControl<boolean | null>>([]);
+
   constructor() {
     this.cardList = cardJSON;
+    this.initializeCheckboxes();
+  }
+
+  initializeCheckboxes() {
+    this.cardList.forEach(() =>
+      this.checkboxArray.push(new FormControl(false))
+    );
+  }
+
+  onCheckboxChange(event: Event, index: number) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    console.log(
+      `Checkbox at position ${index} is ${
+        isChecked ? 'checked' : 'unchecked'
+      } and the budget is ${this.cardList[index].price}`
+    );
+
+    if (isChecked) {
+      this.addBudget(index);
+    } else if (!isChecked) {
+      this.removeBudget(index);
+    }
+    console.log(this.budgetList);
+  }
+
+  addBudget(index: number): void {
+    this.budgetList.push(this.cardList[index].price);
+  }
+
+  removeBudget(index: number): void {
+    this.removedIndex = this.budgetList.indexOf(this.cardList[index].price);
+    if (this.removedIndex !== -1) {
+      this.budgetList.splice(this.removedIndex, 1);
+    }
   }
 }
