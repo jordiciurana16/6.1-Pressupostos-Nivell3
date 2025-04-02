58d5212a79c5e0270a19c545a906c0c5d8d7bb41
jordiciurana16 <jciuranas@gmail.com>
2025-04-02 13:17:19 +0200

    panel component conditional render

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/card/card.component.ts
@@ -4,10 +4,11 @@ import cardJSON from './cards.json';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormArray, FormControl } from '@angular/forms';
 import { BudgetService } from '../budget.service';
+import { PanelComponent } from '../panel/panel.component';
 
 @Component({
   selector: 'app-card',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, PanelComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
@@ -15,6 +16,8 @@ export class CardComponent {
   cardList: ICard[] = [];
 
   checkboxArray = new FormArray<FormControl<boolean | null>>([]);
+
+  webCardIsChecked = false;
 
   constructor(private BudgetService: BudgetService) {
     this.cardList = cardJSON;
@@ -29,9 +32,14 @@ export class CardComponent {
 
   onCheckboxChange(event: Event, index: number) {
     const isChecked = (event.target as HTMLInputElement).checked;
+
     this.BudgetService.handleBudgetChange(
       this.cardList[index].price,
       isChecked
     );
+
+    if (isChecked && index === this.cardList.length - 1) {
+      this.webCardIsChecked = true;
+    }
   }
 }
