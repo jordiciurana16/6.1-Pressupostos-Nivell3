c456e790323e449a3b0518fa63cc1a1067a7721e
jordiciurana16 <jciuranas@gmail.com>
2025-04-02 15:57:23 +0200

    update cotnrols buttons

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/panel/panel.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/panel/panel.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './panel.component.html',
-  styleUrl: './panel.component.scss'
+  styleUrl: './panel.component.scss',
 })
 export class PanelComponent {
+  pages = new FormControl(1);
+  languages = new FormControl(1);
+  target: any;
 
+  constructor() {}
+
+  updateValue(control: FormControl, value: boolean) {
+    const inputValue = control.value;
+
+    value ? control.setValue(inputValue + 1) : control.setValue(inputValue - 1);
+  }
 }
