a088d2210b93890d00700c7a48a325de99e9deba
jordiciurana16 <jciuranas@gmail.com>
2025-04-05 00:30:59 +0200

    icones de bootstrap i bootstrap import

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/panel/panel.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/panel/panel.component.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-
 import { BudgetService } from '../budget.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-panel',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss',
 })
 export class PanelComponent {
   pages = new FormControl(1);
   languages = new FormControl(1);
-  target: any;
 
   constructor(public BudgetService: BudgetService) {}
 
   updateValue(control: FormControl, value: boolean) {
     const inputValue = control.value;
-
     if (value) {
       control.setValue(inputValue + 1);
     } else if (!value && inputValue > 1) {
