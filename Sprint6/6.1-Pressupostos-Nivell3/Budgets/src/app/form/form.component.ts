d93e8f5e08c389848ed2efa4a08e017788414887
jordiciurana16 <jciuranas@gmail.com>
2025-04-06 23:59:17 +0200

    form user budget price + service names

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/form/form.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/form/form.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BudgetService } from '../budget.service';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
 export class FormComponent {
-  userObject = {};
+  userObject:
+    | {
+        name: string;
+        phone: number;
+        mail: string;
+        price: number;
+        services: Array<string>;
+      }
+    | undefined;
   usersBudgetList: any = [];
   RequireBudgetForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    public BudgetService: BudgetService
+  ) {}
   ngOnInit() {
     this.RequireBudgetForm = this.formBuilder.group({
       name: new FormControl(''),
@@ -23,8 +36,21 @@ export class FormComponent {
   }
 
   onSubmit() {
-    this.userObject = this.RequireBudgetForm.value;
-    this.usersBudgetList.push(this.userObject);
-    console.log(this.usersBudgetList);
+    if (!this.userObject) {
+      this.userObject = {
+        name: '',
+        phone: 0,
+        mail: '',
+        price: 0,
+        services: [],
+      };
+    }
+    this.userObject.name = this.RequireBudgetForm.get('name')?.value;
+    this.userObject.phone = this.RequireBudgetForm.get('phone')?.value;
+    this.userObject.mail = this.RequireBudgetForm.get('mail')?.value;
+    this.userObject.price = this.BudgetService.getTotalBudget();
+    this.userObject.services = this.BudgetService.serviceNamesList;
+
+    this.usersBudgetList.push({ ...this.userObject });
   }
 }
