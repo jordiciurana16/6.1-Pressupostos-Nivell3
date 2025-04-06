f183afb9e3908ef3b2414deced871fdb6889bc10
jordiciurana16 <jciuranas@gmail.com>
2025-04-06 14:49:37 +0200

    demanar pressupost layout

--- Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/landing/landing.component.ts
+++ Sprint6/6.1-Pressupostos-Nivell3/Budgets/src/app/landing/landing.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../banner/banner.component';
 import { BudgetsComponent } from '../budgets/budgets.component';
 import { RouterModule, RouterLink } from '@angular/router';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [BannerComponent, BudgetsComponent, RouterLink, RouterModule],
+  imports: [
+    BannerComponent,
+    BudgetsComponent,
+    RouterLink,
+    RouterModule,
+    FormComponent,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
