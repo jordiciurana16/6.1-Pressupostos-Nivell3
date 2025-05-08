e9ff2775612c9b4b46ad7ba6223c4e17e3056d94
jordiciurana16 <jciuranas@gmail.com>
2025-05-08 12:41:19 +0200

    api endpoint request

--- Sprint7/star-wars/src/app/components/nav/nav.component.ts
+++ Sprint7/star-wars/src/app/components/nav/nav.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../service/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  providers: [ApiService, CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
-
+  constructor(private apiService: ApiService) {}
+  starships: any[] = [];
+  starshipsPage() {
+    this.apiService.getStarships().subscribe((data) => {
+      console.log('Starships data:', data); // Mostra el resultat per consola
+      this.starships = data;
+    });
+  }
 }
