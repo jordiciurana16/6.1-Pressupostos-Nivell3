d64caf4298a93bbad4ea7386fc0e80cd820f909b
jordiciurana16 <jciuranas@gmail.com>
2025-05-08 11:46:38 +0200

    header compoenent layout

--- Sprint7/star-wars/src/app/app.component.ts
+++ Sprint7/star-wars/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'star-wars';
